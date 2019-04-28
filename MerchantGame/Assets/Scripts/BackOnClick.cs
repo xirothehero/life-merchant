2f8fbc3bdc2d6a79577f79781e109938d7493098
Ali Kedwaii <ali.kedwaii@utexas.edu>
2019-04-28 15:42:52 -0500

    add art templates, working on shop menu functionality

---
+++ MerchantGame/Assets/Scripts/BackOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackOnClick : MonoBehaviour
+{
+    public Button BackBtn;
+    public GameObject Panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = BackBtn.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void OnClick(){
+        if (Panel != null){
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+}
---
+++ MerchantGame/Assets/Scripts/BuyOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyOnClick : MonoBehaviour
+{
+    public Button buyBtn;
+    public GameObject Panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = buyBtn.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void OnClick(){
+        if (Panel != null){
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+}
