f76d77287c80c0128b02db9c75f2e733230f4a66
Jake <jakercrabtree@utexas.edu>
2019-04-29 19:28:47 -0500

    audiomanager 2

--- MerchantGame/Assets/Scripts/AudioManager.cs
+++ MerchantGame/Assets/Scripts/AudioManager.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+
+    AudioSource[] sources;
+    public List<AudioClip> musicClips;
+    public List<AudioClip> soundClips;
+
+    public enum SoundType {Sound, Music};
+    public enum MusicClipName{BackgroundMusic, BattleMusic}; 
+    public enum SoundClipName{MagicMist, SwordStrike, Death}; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sources = GetComponents<AudioSource>(); 
+    }
+
+    
+    public void PlayMusic(MusicClipName name){
+        sources[(int)SoundType.Music].clip = musicClips[(int)name];
+        sources[(int)SoundType.Music].Play();
+    }
+    public void PlaySound(SoundClipName name){
+        sources[(int)SoundType.Sound].clip = soundClips[(int)name];
+        sources[(int)SoundType.Sound].Play();
     }
 
     // Update is called once per frame
